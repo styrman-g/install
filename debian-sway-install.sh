3722a92814f9a1b72968f60440db149e2ae8494e
Gustav Hogbring <gustav.hogbring@proton.me>
2026-02-14 10:39:01 +0100

    update

--- debian-sway-install.sh
+++ debian-sway-install.sh
@@ -7,7 +7,7 @@ sudo apt update && sudo apt upgrade
 
 # Installing important programs first
 echo "Installing basic programs"
-sudo apt install git stow
+sudo apt install git stow zip unzip
 
 # Installing sway
 echo "Do you want to install SWAY? (yes/no)"
@@ -52,10 +52,14 @@ read answer
 
 if [ "$answer" = "yes" ]; then
     echo "Cloning my dotfiles"
+    cd
     git clone https://github.com/styrman-g/dotfiles.git
     rm -r .bashrc
     cd dotfiles
     stow .
+    sudo apt install lsd curl
+    cd
+    curl -sS https://starship.rs/install.sh | sh
 elif [ "$answer" = "no" ]; then
     echo "Dont want styrmans dotfiles???? ok."
 else
@@ -72,6 +76,14 @@ if [ "$answer" = "doom" ]; then
     echo "Installing Doom"
     git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
     ~/.config/emacs/bin/doom install
+    cd
+    rm -rf .bashrc
+    cd .config
+    rm -rf /doom
+    cd
+    cd dotfiles
+    stow .
+    cd
     doom sync
 elif [ "$answer" = "no" ]; then
     echo "Setting up emacs later"
@@ -80,13 +92,43 @@ else
 fi
 
 # Install fonts
-# ställ fråga om installera
+echo "Do you want to install styrmans fonts? (y/n)"
+read answer
 
-# Install themes
-# ställa fråga
+if ["$answer" = "y" ]; then
+    # 1. Create font directory
+    mkdir -p ~/.local/share/fonts
+    cd
+    # 2. Download the font (JetBrainsMono Nerd Font)
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+    # 3. Unzip the file
+    unzip JetBrainsMono.zip -d ~/.local/share/fonts
+    # 4. Remove the zip file
+    rm JetBrainsMono.zip
+    # 5. Refresh font cache
+    fc-cache -fv
+elif ["$answer" = "n" ];then
+    echo "No fonts installed"
+else
+    echo "OK"
+fi
 
 # Install nwg-look
-# ställ fråga
+echo "Do you want to install nwg-look for setting themes? (y/n)"
+read answer
+
+if ["$answer" = "y" ];then
+    echo "Installing dependencies"
+    sudo apt install git build-essential lxqt
+    cd
+    git clone https://github.com/Nwg-piotr/nwg-look.git
+    cd nwg-look
+    ./install.sh
+elif ["$answer" = "n" ]; then
+    echo "nwg-look is not installed"
+else
+    echo "OK"
+fi
 
 # Install flatpaks
 # ställ fråga
